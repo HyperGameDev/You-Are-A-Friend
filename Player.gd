d9009d161e41848ead5bbbcfc7322c75dc240671
Lýz <dev@lyzldy.live>
2023-10-05 13:29:35 -0500

    Test

---
+++ Player.gd
@@ -0,0 +1,66 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var look_direction: Vector2
+@onready var camera = $Camera3D
+var camera_sensitivity = 50
+
+var capMouse = true
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	
+	if Input.is_action_just_pressed("pause"):
+		capMouse = !capMouse
+	
+		if capMouse:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			
+	_rotate_camera(delta)
+	move_and_slide()
+
+func _input(event: InputEvent):
+	
+	# Mouse Input (Looking)
+	if event is InputEventMouseMotion: look_direction = event.relative * 0.01
+
+func _rotate_camera(delta: float, sensitivity_y_mod: float = .6, sensitivity_x_mod: float = .6):
+	
+	# Joypad Input (Looking) - 
+	var input = Input.get_vector("look_left", "look_right", "look_down", "look_up")
+	input.y *= -1
+	look_direction += input * .1
+	
+	# Left-Right looking
+	rotation.y -= look_direction.x * camera_sensitivity * sensitivity_x_mod * delta
+	
+	# Up-Down looking
+	camera.rotation.x = clamp(camera.rotation.x - look_direction.y * camera_sensitivity * sensitivity_y_mod * delta, -1.5, 1.5)
+	
+	# Default look position
+	look_direction = Vector2.ZERO
---
+++ friend.gd
@@ -0,0 +1,53 @@
+extends StaticBody3D
+
+@export var friend_name = "Friend"
+
+var convo_state = 0
+var friend_hover = false
+var dialogue_hover = true
+var dialogue_close_hover = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"Dialogue/Friend Dialogue/Friend Name".set_text(friend_name)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_released("Mouse") and friend_hover == true:
+		$"Dialogue".visible = true
+		input_ray_pickable = false
+		convo_state = 1
+		
+	if $"Dialogue".visible:
+		await get_tree().create_timer(0.1).timeout
+		if Input.is_action_just_released("Mouse") and dialogue_close_hover == true:
+			$"Dialogue".visible = false
+			dialogue_close_hover = false
+			convo_state = 0
+			input_ray_pickable = true
+		
+func _on_mouse_entered():
+	if convo_state == 0:
+		friend_hover = true
+
+func _on_mouse_exited():
+	friend_hover = false
+
+func _on_close_panel_top_mouse_entered():
+	dialogue_close_hover = true
+
+func _on_close_panel_l_mouse_entered():
+	dialogue_close_hover = true
+	
+func _on_close_panel_r_mouse_entered():
+	dialogue_close_hover = true
+
+func _on_close_panel_top_mouse_exited():
+	dialogue_close_hover = false
+
+func _on_close_panel_l_mouse_exited():
+	dialogue_close_hover = false
+
+func _on_close_panel_r_mouse_exited():
+	dialogue_close_hover = false
