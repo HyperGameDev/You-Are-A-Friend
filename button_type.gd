b542d50f01c516a4783ce518075da52b8b9b10c0
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2023-10-19 17:18:25 -0500

    Attempt to add texture export var
    Saving before replacing panels of buttons with texturebutton

--- button_type.gd
+++ button_type.gd
@@ -1,6 +1,7 @@
 extends Panel
 
-@export var button_type = "Label"
+@export var button_type_label = "Label"
+@export var button_type_texture = load("res://UI images/border_responses_4.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +10,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	%"Type Label".text = button_type
+	%"Type Label".text = button_type_label
