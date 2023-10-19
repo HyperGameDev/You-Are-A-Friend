78d09bb552a843127aff841dffe763a8f714d54c
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2023-10-19 14:59:38 -0500

    Salvaged Lost Progress
    Detatched head from last week prevented me from keeping recent commits, this is a less complete version of the progress since then

--- Dialogue/msg_type.gd
+++ Dialogue/msg_type.gd
@@ -64,17 +64,17 @@ func _hover_states():
 	# Applying enum array to button sizes	
 	match msg_type_hover_state:
 		Msg_Meta_Hover["EAGER"]:
-			%CenterContainer.scale = Vector2(1.5,1.5)
+			%"Button Eager".scale = Vector2(1.5,1.5)
 			%"Button Even".scale = Vector2(1,1)
 			%"Button Distant".scale = Vector2(1,1)
-			%"Meta Responses".position.x = %"Button Eager".global_position.x - 65
+#			%"Meta Responses".position.x = %"Button Eager".global_position.x - 65
 		Msg_Meta_Hover["EVEN"]:
 			%"Button Eager".scale = Vector2(1,1)
 			%"Button Even".scale = Vector2(1.5,1.5)
 			%"Button Distant".scale = Vector2(1,1)
-			%"Meta Responses".position.x = %"Button Even".global_position.x - 65
+#			%"Meta Responses".position.x = %"Button Even".global_position.x - 65
 		Msg_Meta_Hover["DISTANT"]:
 			%"Button Eager".scale = Vector2(1,1)
 			%"Button Even".scale = Vector2(1,1)
 			%"Button Distant".scale = Vector2(1.5,1.5)
-			%"Meta Responses".position.x = %"Button Distant".global_position.x - 65
+#			%"Meta Responses".position.x = %"Button Distant".global_position.x - 65
